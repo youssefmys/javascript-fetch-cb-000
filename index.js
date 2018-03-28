8bb65c1819d0a50635a055de3599dc184a8c76c6
youssefmys <mosa500@gmail.com>
2018-03-28 02:57:13 +0200

    Done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+
 const app = "I don't do much.";
+
+let xhr = new XMLHttpRequest()
+xhr.open('GET', 'https://api.github.com/repos/jquery/jquery/commits');
+
+xhr.response_type = 'json'
+
+xhr.onload = function(){
+  console.log(xhr.response)
+}
+xhr.onerror = function() {
+  console.log('Boo')
+}
+
+xhr.send()
+
+
+fetch('https://api.github.com/repos/jquery/jquery/commits').then(res => res.json()).then(json => console.log(json))
+
+const token = "test_token"
+
+fetch('https://api.github.com/users/youssefmys/repos', {
+  header: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json))
